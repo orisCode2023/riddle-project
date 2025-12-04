76ecf75d134253ed4af7fbb19525d334d46434bb
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-04 10:39:58 +0200

    adding another property to the riddle to get the answer option

--- riddles/r2.js
+++ riddles/r2.js
@@ -2,5 +2,6 @@ const riddle = {
     id: 2,
     name: "geuss noise",
     taskDescription:"which of the following animal is roar" ,
+    choices  : ["dog", "cat", "sheep", "lion"],
     correctAnswer:"lion"
 }
