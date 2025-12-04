65cdcd18f25f0fb626f284397d25e4c2abf97dc8
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-04 12:44:20 +0200

    project completed

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 import input from "analiza-sync";
-import r1 from "./riddles/r1.js";
-import r2 from "./riddles/r2.js";
+import riddles from "./riddles/all-riddles.js";
 import game from "./core/game.js";
-import player from "./core/player.js";
+import playerFile from "./core/player.js";
 
+function init(){
+    console.log("Welcome to Riddle Game")
+    const name = input("Plaese enter your name: ")
+    return name    
+}
+
+
+function play(arr=[Object], player=Object){
+    for (let i = 0; i < arr.length; i++){
+        playerFile.addSolveTime(player, game.measureSolveTime(()=> game.askRiddle(arr[i])))
+    }
+    console.log("Great Job, you hit the finish line")
+    playerFile.showStats(player)
+}
+
+play(riddles, playerFile.createPlayer(init()))
