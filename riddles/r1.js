eb527359f59a0b6509c1bcf1cf01989abbe545c9
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-04 09:55:22 +0200

    first riddle ready

--- riddles/r1.js
+++ riddles/r1.js
@@ -1,6 +1,6 @@
 const riddle = {
-    id: 0,
-    name: "String",
-    taskDescription:"String" ,
-    correctAnswer:"String"
+    id: 1,
+    name: "Simple Math",
+    taskDescription:"what is the square of a 3" ,
+    correctAnswer:"9"
 }
