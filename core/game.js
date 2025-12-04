e921971e804d87bb9be0049f9aa13bb161f7e3b0
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-04 11:23:39 +0200

    adding a ask function to ask the riddle and to get the answer

--- core/game.js
+++ core/game.js
@@ -1,7 +1,31 @@
-function askRiddle(riddleObj){
-    return null
+import input from "analiza-sync";
+
+function askRiddle(riddleObj = Object) {
+    console.log(`The riddle name is: ${riddleObj.name}`)
+    console.log(`The riddle is: ${riddleObj.taskDescription}`)
+    while (true) {
+        if (riddleObj.hasOwnProperty("choices")) {
+            console.log(riddleObj.choices)
+            const answerChoice = input("Enter your choice in numbers ")
+            if (answerChoice === String(riddleObj.choices.indexOf(riddleObj.correctAnswer) + 1)) {
+                console.log("Correct!")
+                break
+            } else {
+                console.log("Not Correct! Try again")
+            }
+        } else {
+        const answer = input("Enter your answer: ")
+        if (answer === riddleObj.correctAnswer) {
+            console.log("Correct!")
+            break
+        } else {
+            console.log("Not Correct! Try again")
+        }
+    }
+    } 
 }
 
-function  measureSolveTime(fn){
+function measureSolveTime(fn) {
     return Number
 }
+
